2acdb144e15e2a6eba93508e5de34ab3510acf7c
Tamara <shanachebet0@gmail.com>
2025-11-24 14:51:43 +0300

    JavaScript expressions

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+//Temperatures for each day
+const day1TempF = 32
+const day2TempC = 25
+const day3TempF = 70
+const day4TempC = 18
+const day5TempF = 80
+const day6TempC = 15
+const day7TempF = 72
+const day8TempC = 28
+const day9TempF = 68
+const day10TempC = 20
+const day11TempF = 75
+const day12TempC = 23
+const day13TempF = 82
+const day14TempC = 30
+const day15TempF = 65
+const day16TempC = 22
+const day17TempF = 77
+const day18TempC =26
+const day19TempF = 78
+const day20TempC = 24
+const day21TempF = 73
+const day22TempC = 21
+const day23TempF = 79
+const day24TempC = 27
+const day25TempF = 71
+const day26TempC = 19
+const day27TempF = 74
+const day28TempC = 17
+const day29TempF = 76
+const day30TempC = 29
 
+//Temperature for each day in fahrenheit
+const tempF =[32,70,80,72,68,75,82,65,77,78,73,79,71,74,76]; 
 
-//! Start the calculation of the total temperatures
+//Convert fahrenheit to celsius
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+// output: fahrenheitToCelsius(68); // 20 
+//Temperature for each day in celsius
+const tempC = [25,18,15,28,20,23,30,22,26,24,21,27,19,17,29];
+
+//Convert celsius to fahrenheit
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+    console.log(celsiusToFahrenheit(20)); // 68
+}
+
+// sum up all the temperatures in fahrenheit
+function tot_temperature_in_fahrenheit(){
+    return tempF.reduce((sum, t) => sum + t, 0);
+}
+
+// sum of all tempreaature in celcius
+function tot_temperature_in_celsius(){
+    return tempC.reduce((sum, t) => sum + t, 0);
+}
+
+// calculate average temperature in fahrenheit
+function avg_temperature_in_fahrenheit(){
+    return tot_temperature_in_fahrenheit() / tempF.length;
+}
+
+// calculate average temperature in celius
+function avg_temperature_in_celsius(){
+    return tot_temperature_in_celsius() / tempC.length;
+}
+
+console.log("Total Temperature in Fahrenheit: " + tot_temperature_in_fahrenheit());
+console.log("Total Temperature in Celsius: " + tot_temperature_in_celsius());
+console.log("Average Temperature in Fahrenheit: " + avg_temperature_in_fahrenheit());
+console.log("Average Temperature in Celsius: " + avg_temperature_in_celsius());
+
 //* Then apply the conversion to calculate the total in the other unit of measurement
 //* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
 
