fd1d90d870ea9733cadecf035fc7fb7540e50303
Tamara <shanachebet0@gmail.com>
2025-11-24 15:22:02 +0300

    javascripts expressions

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const tempF =[32,70,80,72,68,75,82,65,77,78,73,79,71,74,76];
 
 //Convert fahrenheit to celsius
 function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - 32) * (5 / 9);
 }
 
 // output: fahrenheitToCelsius(68); // 20 
@@ -45,33 +45,45 @@ const tempC = [25,18,15,28,20,23,30,22,26,24,21,27,19,17,29];
 //Convert celsius to fahrenheit
 function celsiusToFahrenheit(celsius) {
     return (celsius * 9 / 5) + 32;
-    console.log(celsiusToFahrenheit(20)); // 68
 }
+// console.log(celsiusToFahrenheit(20)); // 68
 
 // sum up all the temperatures in fahrenheit
-function tot_temperature_in_fahrenheit(){
-    return tempF.reduce((sum, t) => sum + t, 0);
+function calc_tot_temperature_in_fahrenheit(){
+    const sumF = tempF.reduce((sum, t) => sum + t, 0);
+    const sumFromC = tempC.reduce((sum, c) => sum + celsiusToFahrenheit(c), 0);
+    return sumF + sumFromC;
 }
 
 // sum of all tempreaature in celcius
-function tot_temperature_in_celsius(){
-    return tempC.reduce((sum, t) => sum + t, 0);
+function calc_tot_temperature_in_celsius(){
+    const sumC = tempC.reduce((sum, t) => sum + t, 0);
+    const sumFromF = tempF.reduce((sum, f) => sum + fahrenheitToCelsius(f), 0);
+    return sumC + sumFromF;
 }
 
 // calculate average temperature in fahrenheit
-function avg_temperature_in_fahrenheit(){
-    return tot_temperature_in_fahrenheit() / tempF.length;
+function calc_avg_temperature_in_fahrenheit(){
+    const totalDays = tempF.length + tempC.length;
+    return calc_tot_temperature_in_fahrenheit() / totalDays;
 }
 
-// calculate average temperature in celius
-function avg_temperature_in_celsius(){
-    return tot_temperature_in_celsius() / tempC.length;
+// calculate average temperature in celsius
+function calc_avg_temperature_in_celsius(){
+    const totalDays = tempF.length + tempC.length;
+    return calc_tot_temperature_in_celsius() / totalDays;
 }
 
-console.log("Total Temperature in Fahrenheit: " + tot_temperature_in_fahrenheit());
-console.log("Total Temperature in Celsius: " + tot_temperature_in_celsius());
-console.log("Average Temperature in Fahrenheit: " + avg_temperature_in_fahrenheit());
-console.log("Average Temperature in Celsius: " + avg_temperature_in_celsius());
+// create the exported numeric variables expected by the tests
+const tot_temperature_in_fahrenheit = calc_tot_temperature_in_fahrenheit();
+const tot_temperature_in_celsius = calc_tot_temperature_in_celsius();
+const avg_temperature_in_fahrenheit = calc_avg_temperature_in_fahrenheit();
+const avg_temperature_in_celsius = calc_avg_temperature_in_celsius();
+
+console.log("Total Temperature in Fahrenheit: " + tot_temperature_in_fahrenheit);
+console.log("Total Temperature in Celsius: " + tot_temperature_in_celsius);
+console.log("Average Temperature in Fahrenheit: " + avg_temperature_in_fahrenheit);
+console.log("Average Temperature in Celsius: " + avg_temperature_in_celsius);
 
 //* Then apply the conversion to calculate the total in the other unit of measurement
 //* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
@@ -85,8 +97,8 @@ console.log("Average Temperature in Celsius: " + avg_temperature_in_celsius());
 //* This way you can export them to the test file, this is essential for the tests to work
 
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
+    tot_temperature_in_fahrenheit,
+    tot_temperature_in_celsius,
+    avg_temperature_in_fahrenheit,
+    avg_temperature_in_celsius
 };
